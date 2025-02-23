3a7c3b93219fb6a229399089d5c3734762c88e08
yyjeqhc <1772413353@qq.com>
2025-02-23 16:03:00 +0800

    update

--- exercises/easy/algorithm17.rs
+++ exercises/easy/algorithm17.rs
@@ -16,7 +16,8 @@ pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
     let a:HashSet<_> = nums1.into_iter().collect();
     let b:HashSet<_> = nums2.into_iter().collect();
     
-    let intersection: Vec<i32> = a.intersection(&b).map(|v|*v).collect();
+    let mut intersection: Vec<i32> = a.intersection(&b).map(|v|*v).collect();
+    intersection.sort();
     intersection
 }
 
